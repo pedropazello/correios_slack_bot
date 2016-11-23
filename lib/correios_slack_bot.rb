3d9bd5be7ffcbb8a5542fb124384eb0327a18ecf
Pedro Pazello <pedropazello.rj@gmail.com>
2016-11-23 21:31:20 -0200

    Extract correios_slack_bot/bot to main class

--- lib/correios_slack_bot.rb
+++ lib/correios_slack_bot.rb
@@ -1,5 +1,22 @@
 require "correios_slack_bot/version"
+require "correios_slack_bot/correios"
+require "slack-ruby-bot"
 
 module CorreiosSlackBot
-  # Your code goes here...
+  class Bot < SlackRubyBot::Bot
+    match /pacote (?<package_code>\w*)$/ do |client, data, match|
+      package_log = CorreiosSlackBot::Correios.new(match[:package_code]).package_log
+      result = package_log.map do |log|
+        {
+          title: "#{log[:date]} - #{log[:origin]}",
+          text: "#{log[:description]} (#{log[:status]})"
+        }
+      end
+      client.web_client.chat_postMessage(
+        channel: data.channel,
+        as_user: true,
+        attachments: result
+      )
+    end
+  end
 end
