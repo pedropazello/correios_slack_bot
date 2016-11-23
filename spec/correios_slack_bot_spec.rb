326b5520baffaa63631a374d87f9d9e01a2949af
Pedro Pazello <pedropazello.rj@gmail.com>
2016-11-23 19:53:03 -0200

    Remove invalid test

--- spec/correios_slack_bot_spec.rb
+++ spec/correios_slack_bot_spec.rb
@@ -4,8 +4,4 @@ describe CorreiosSlackBot do
   it "has a version number" do
     expect(CorreiosSlackBot::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
