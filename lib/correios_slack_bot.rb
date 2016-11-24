294e87756f0b393f64bab03f9c891f818984f776
Pedro Pazello <pedropazello.rj@gmail.com>
2016-11-24 21:01:51 -0200

    Implement a basic package monitor feature, need refactoring e improvments

--- lib/correios_slack_bot.rb
+++ lib/correios_slack_bot.rb
@@ -20,6 +20,28 @@ module CorreiosSlackBot
     end
 
     match /^rastreia o pacote (?<package_code>\w*)$/ do |client, data, match|
+      package_log = CorreiosSlackBot::Correios.new(match[:package_code]).package_log
+      known_package_log = package_log
+      client.say(text: 'Blz, vou começar a rastrar, qualquer coisa te falo',
+        channel: data.channel)
+      loop do
+        new_package_log = CorreiosSlackBot::Correios.new(match[:package_code]).package_log
+        if (new_package_log - known_package_log).size > 0
+          log = new_package_log[0]
+          client.web_client.chat_postMessage(
+            channel: data.channel,
+            as_user: true,
+            attachments: [
+              {
+                title: "#{log[:date]} - #{log[:origin]}",
+                text: "#{log[:description]} (#{log[:status]})"
+              }
+            ]
+          )
+          known_package_log = new_package_log
+        end
+        sleep 30
+      end
     end
   end
 end
